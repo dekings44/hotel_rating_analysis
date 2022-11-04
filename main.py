d3168aa83d338b14dd47bcf03543c419e455477e
Kingsley Ihemere <dekings44@gmail.com>
2022-11-04 23:14:37 +0000

    added new query parameter

--- main.py
+++ main.py
@@ -14,33 +14,36 @@ import plotly.express as px
 
 # url = 'https://www.tripadvisor.co.uk/Hotels-g191-United_States-Hotels.html'
 
-url = 'https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1FCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQzYAQHoAQH4AQuIAgGoAgO4AtLB_JEGwAIB0gIkZDg0Y2M3MjYtZjc0Yy00OTI0LWFmNTEtNzFhOTc3MTY0ZjBk2AIG4AIB&sid=828d71aca926f8532f0cb1f47b533b81&aid=304142&ss=London&ssne=London&ssne_untouched=London&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=-2601889&dest_type=city&checkin=2022-07-16&checkout=2022-07-17&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure&offset='
+url = "https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1DCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4Aoq1lpsGwAIB0gIkZjA3N2ZjM2YtMzkzNi00ZDYyLWIzZTQtMTJjZGMyYTlhMWM02AIE4AIB&sid=828d71aca926f8532f0cb1f47b533b81&sb=1&sb_lp=1&src=index&src_elem=sb&error_url=https%3A%2F%2Fwww.booking.com%2Findex.en-gb.html%3Flabel%3Dgen173nr-1DCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQzYAQPoAQGIAgGoAgO4Aoq1lpsGwAIB0gIkZjA3N2ZjM2YtMzkzNi00ZDYyLWIzZTQtMTJjZGMyYTlhMWM02AIE4AIB%26sid%3D828d71aca926f8532f0cb1f47b533b81%26sb_price_type%3Dtotal%26%26&ss=London%2C+Greater+London%2C+United+Kingdom&is_ski_area=&ssne=Paris&ssne_untouched=Paris&checkin_year=2022&checkin_month=12&checkin_monthday=27&checkout_year=2022&checkout_month=12&checkout_monthday=28&efdco=1&group_adults=2&group_children=0&no_rooms=1&b_h4u_keep_filters=&from_sf=1&search_pageview_id=e115a245801601aa&ac_suggestion_list_length=5&ac_suggestion_theme_list_length=0&ac_position=1&ac_langcode=en&ac_click_type=b&ac_meta=GhBlMTE1YTI0NTgwMTYwMWFhIAEoATICZW46BkxvbmRvbkAASgBQAA%3D%3D&dest_id=-2601889&dest_type=city&iata=LON&place_id_lat=51.507393&place_id_lon=-0.127634&search_pageview_id=e115a245801601aa&search_selected=true&ss_raw=London"
 
 header = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
 
-get_page = requests.get(url + str(url) + , headers=header)
+#get_page = requests.get(url + str(url) + , headers=header)
+
+get_page = requests.get(url, headers=header)
 
 print(get_page.status_code)
 
 response = get_page.content
 
-# print(len(response))
+
+print(len(response))
 
 webpage_data = BeautifulSoup(response, 'html5lib')
 
 
 
-pages = np.arange(0, 615, 25)
+# pages = np.arange(0, 615, 25)
 
-for page in pages: 
+# for page in pages: 
   
-  page = requests.get("https://www.booking.com/searchresults.en-gb.html?label=gen173nr-1FCAEoggI46AdIM1gEaFCIAQGYAQm4ARfIAQzYAQHoAQH4AQuIAgGoAgO4AtLB_JEGwAIB0gIkZDg0Y2M3MjYtZjc0Yy00OTI0LWFmNTEtNzFhOTc3MTY0ZjBk2AIG4AIB&sid=828d71aca926f8532f0cb1f47b533b81&aid=304142&ss=London&ssne=London&ssne_untouched=London&lang=en-gb&sb=1&src_elem=sb&src=searchresults&dest_id=-2601889&dest_type=city&checkin=2022-07-16&checkout=2022-07-17&group_adults=2&no_rooms=1&group_children=0&sb_travel_purpose=leisure&offset=" + str(page), headers=header)
+#   page = requests.get("" + str(page), headers=header)
   
-  soup = BeautifulSoup(response, 'html5lib')
+#   soup = BeautifulSoup(response, 'html5lib')
   
-  movie_div = soup.find_all('div', class_='lister-item mode-advanced')
+#   movie_div = soup.find_all('div', class_='lister-item mode-advanced')
   
-  sleep(randint(2,10))
+#   sleep(randint(2,10))
 
 # print(webpage_data)
 
@@ -48,6 +51,7 @@ for page in pages:
 # for name in webpage_data.findAll('div', {'data-testid' : 'title'}):
 #     hotel.append(name.text)
 #     print(hotel)
+
 
 # pizza = webpage_data.find_all('a', {'class' : 'css-1m051bw'})
 
